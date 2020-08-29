036ebfaa5b59a5651aaedcf593d8eef9d89438b9
Andrii <kulchaka@gmail.com>
2020-08-29 16:57:02 +0200

    add some changes is project

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ const WebpackMd5Hash = require('webpack-md5-hash');
 const webpack = require('webpack');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 // подключаем плагин
-const isDev = process.env.NODE_ENV === 'development';
-// создаем переменную для development-сборки
+const isDev = process.env.NODE_ENV === 'development' ? 'http://praktikum.tk/cohort2' : 'https://praktikum.tk/cohort2';
+// const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort2' : 'https://praktikum.tk/cohort2';
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/js/script.js'),
@@ -29,13 +29,40 @@ module.exports = {
                 test: /\.css$/i,
                 use: [
                     (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                    'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2
+                        }
+                    },
                     'postcss-loader'
                 ]
             },
             {
                 test: /\.(png|jpg|gif|ico|svg)$/,
-                use: 'file-loader?name=./images/[name].[ext]&esModule=false'
+                use: [
+                    'file-loader?name=./images/[name].[ext]&esModule=false',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            }
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(eot|ttf|woff|woff2)$/,
